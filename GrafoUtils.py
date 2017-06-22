ff2cf80c1511504906a13d927ba334e121c9d5e0
NKeklikian <nkeklikian@gmail.com>
2017-06-22 10:23:04 -0300

    Corrijo funciones generadoras

--- GrafoUtils.py
+++ GrafoUtils.py
@@ -50,22 +50,23 @@ class DigrafoConPeso(Digrafo):
     
 def generarGrafoConexo(n): # n vertices, 2n aristas
 	from random import seed, randint
-    seed()
-    grafo = Grafo(n)
-    for v in xrange(n):
-        u = randint(0, v-1)
-        grafo.eje(u, v)
-    for _ in xrange(n):
-        u, v = randint(0, n-1), randint(0, n-1)
-        while u == v or u in grafo.vecinos(v):
-            v = randint(0, n-1)
-        grafo.eje(u, v)
-    return grafo
+	seed()
+	grafo = Grafo(n)
+	for v in xrange(1, n):
+		u = randint(0, v-1)
+		grafo.eje(u, v)
+	for _ in xrange(n+1):
+		u, v = randint(0, n-1), randint(0, n-1)
+		while u == v or u in grafo.vecinos(v):
+			v = randint(0, n-1)
+		grafo.eje(u, v)
+	return grafo
 
 def generarSubsetSum(n): # devuelve una tupla (gen_n, t) donde gen_n es un generador del conjunto
                          # y t es el valor a aproximar
-    from random import seed, randint
-    seed()
-    t = randint(10**8, 10**9)
-    return ((randint(1, t-1) for _ in xrange(n)), t) # me parecio mejor dejar un generador en vez de una lista
-                                                     # para no desperdiciar tiempo de procesamiento ni memoria
+	from random import seed, randint
+	seed()
+	t = randint(10**8, 10**9)
+	return ((randint(1, t-1) for _ in xrange(n)), t) # me parecio mejor dejar un generador en vez de una lista
+                                                         # para no desperdiciar tiempo de procesamiento ni memoria
+
